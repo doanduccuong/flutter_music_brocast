2170a566405adbfeed8fbfc373bc0f1b48ab1adf
doanduccuong <cuong12c12000@gmail.com>
2022-12-08 16:21:33 +0700

    commit remaining file

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,33 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get settings_title => 'Settings';
+
+  @override
+  String get settings_themeMode => 'Theme';
+
+  @override
+  String get settings_themeModeSystem => 'System';
+
+  @override
+  String get settings_themeModeLight => 'Light';
+
+  @override
+  String get settings_themeModeDark => 'Dark';
+
+  @override
+  String get settings_languageVietnamese => 'Tiếng việt';
+
+  @override
+  String get settings_languageEnglish => 'English';
+
+  @override
+  String get settings_language => 'Language';
+
+  @override
+  String get button_signIn => 'Sign In';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_vi.dart
@@ -0,0 +1,33 @@
+import 'app_localizations.dart';
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get settings_title => 'Cài đặt';
+
+  @override
+  String get settings_themeMode => 'Chủ đề';
+
+  @override
+  String get settings_themeModeSystem => 'Hệ thống';
+
+  @override
+  String get settings_themeModeLight => 'Sáng';
+
+  @override
+  String get settings_themeModeDark => 'Tối';
+
+  @override
+  String get settings_languageVietnamese => 'Tiếng việt';
+
+  @override
+  String get settings_languageEnglish => 'English';
+
+  @override
+  String get settings_language => 'Ngôn ngữ';
+
+  @override
+  String get button_signIn => 'Đăng nhập';
+}
